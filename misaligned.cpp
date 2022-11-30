fbaee20e4df0f4692188c25c55b1d070d89910f4
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-30 10:31:41 +0530

    Update misaligned.cpp
    Separated arrays

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char *majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char *minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int printColorMap()
+{
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
 }
 
-int main() {
+int main()
+{
     int result = printColorMap();
     assert(result == 25);
+    assert(result > 0);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
