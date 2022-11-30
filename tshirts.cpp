f469076db549f06be5b907a41a7dbac0274038a3
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-11-30 10:33:56 +0530

    Update tshirts.cpp
    Updated the asserts

--- tshirts.cpp
+++ tshirts.cpp
@@ -15,9 +15,10 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
-    assert(size(38) == 'M');
+    assert(size(38) == 'S');
     assert(size(41) == 'M');
-    assert(size(42) == 'L');
+    assert(size(42) == 'M');
+    assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
