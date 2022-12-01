3a4fac57e931edca8be43479e59247d04a690f01
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-01 09:26:25 +0530

    Update misaligned.cpp
    Added test for pair and num and assert test

--- misaligned.cpp
+++ misaligned.cpp
@@ -4,11 +4,6 @@
 const char *majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char *minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-bool testAlignment(int icheck, int jcheck, const char *minCol, const char *majCol )
-{
-    
-}
-
 int printColorMap()
 {
     int i = 0, j = 0;
@@ -17,17 +12,46 @@ int printColorMap()
         for (j = 0; j < 5; j++)
         {
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-            assert(testAlignment(i, j, majorColor[i], majorColor[j]));// test function 
         }
     }
     return i * j;
+}
+
+void testNumberToPair(int pairNumber,
+                      MajorColor expectedMajor,
+                      MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+bool testPairToNumber(char *major,char * minor, int expectedPairNumber)
+{
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+
+    return true;
+
+    else
+        return false;
+}
+
+bool  assertionTest()
+{
+    for (int )
+    
 }
 
 int main()
 {
     int result = printColorMap();
     assert(result == 25);
-    assert(result > 0);
+    assert(testPairToNumber("Red","Green", 8));
+    assert(testPairToNumber("Yellow","Brown", 19);)
+
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
