de696b7ddba2e453dbf0b2fa1e0d7b35b903eeab
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-01 10:21:49 +0530

    Changed back code

--- alerter.cpp
+++ alerter.cpp
@@ -30,7 +30,7 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount++;
+        alertFailureCount += 0;
         assert(alertFailureCount>0);
     }
 }
