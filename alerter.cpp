39bcec1c13f4f3bad6dcc2c8ea831f7827d304ed
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-07 13:59:53 +0530

    Update alerter.cpp
    Updated with threshold

--- alerter.cpp
+++ alerter.cpp
@@ -38,6 +38,9 @@ void alertInCelcius(float farenheit) {
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    alertInCelcius(200.0);
+    alertInCelcius(199.9);
+    alertInCelcius(200.1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
